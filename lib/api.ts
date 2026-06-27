91f82856a3013b9a40b4392c66a511f6fde27954
Temybrod <temybroder@gmail.com>
2026-06-27 15:16:32 +0100

    api connecction fix

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/v1';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api-production-1d26.up.railway.app/api/v1';
 
 export const api = axios.create({
   baseURL: API_URL,
